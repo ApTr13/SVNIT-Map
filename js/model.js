4808223482db097868ae1047429fced6b2e3bbca
Apurva Tripathi <apurvatripathi13@gmail.com>
2017-01-27 00:52:48 +0530

    Add files via upload

--- js/model.js
+++ js/model.js
@@ -8,7 +8,8 @@ var initialPlaces = [
 		image: "images/gate.jpg",
 		description: "<b>SVNIT</b>, 2nd best engineering college among all NITs across India...",
 		weblink: "http://svnit.ac.in/",
-		linkdata: "Svnit Website"
+		linkdata: "Svnit Website",
+		keyword: "svnit"
 	},
 	{
 		location: {lat: 21.167213,lng: 72.787265},
@@ -16,7 +17,8 @@ var initialPlaces = [
 		image: "images/mtb.jpg",
 		description: "Mega Hostel for girls and female faculties..",
 		weblink: "http://svnit.ac.in/community/hostels/mtb/index.html",
-		linkdata: "MTB Hostel"
+		linkdata: "MTB Hostel",
+		keyword: "mother teresa"
 	},
 	{
 		location: {lat: 21.164255,lng: 72.786098},
@@ -24,7 +26,8 @@ var initialPlaces = [
 		image: "images/cant.jpg",
 		description: "Centre of love, joy and friendship... SVNIT Canteen",
 		weblink: "http://svnit.ac.in/campus/canteen.php",
-		linkdata: "Canteen Page"
+		linkdata: "Canteen Page",
+		keyword: "canteen"
 	},
 	{
 		location: {lat: 21.165302,lng: 72.785065},
@@ -32,7 +35,8 @@ var initialPlaces = [
 		image: "images/admin.jpg",
 		description: "The place from where whole Administration of College is Controlled..",
 		weblink: "http://svnit.ac.in/admission/adm_btech.php",
-		linkdata: "Admission"
+		linkdata: "Admission",
+		keyword: "administration"
 	},
 	{
 		location: {lat: 21.160201,lng: 72.784852},
@@ -40,7 +44,8 @@ var initialPlaces = [
 		image: "images/sac.jpg",
 		description: "Physical Education Section for various sports... with full-fledged gym.",
 		weblink: "http://svnit.ac.in/sac/about-sac.php",
-		linkdata: "Physical Education"
+		linkdata: "Physical Education",
+		keyword: "physical education"
 	},
 	{
 		location: {lat: 21.162840,lng: 72.785203},
@@ -48,7 +53,8 @@ var initialPlaces = [
 		image: "images/nlib.jpg",
 		description: "Beauty of Svnit, its amazing infrastructure... New Library",
 		weblink: "http://svnit.ac.in/resources/library/lib/main.html",
-		linkdata: "Digital Library"
+		linkdata: "Digital Library",
+		keyword: "library"
 	},
 	{
 		location: {lat: 21.160003,lng: 72.788426},
@@ -56,7 +62,8 @@ var initialPlaces = [
 		image: "images/bb.jpg",
 		description: "8-Storey mega Hostel for 3rd year boys and Ph.d students...",
 		weblink: "http://svnit.ac.in/community/hostels/bb/index.html",
-		linkdata: "Bhabha Bhavan Page"
+		linkdata: "Bhabha Bhavan Page",
+		keyword: "bhabha"
 	},
 	{
 		location: {lat: 21.158130,lng: 72.784693},
@@ -64,7 +71,8 @@ var initialPlaces = [
 		image: "images/gajj.jpg",
 		description: "Home for the freshers of the college...",
 		weblink: "http://svnit.ac.in/community/hostels/gb/index.html",
-		linkdata: "Gajjar Bhavan Page"
+		linkdata: "Gajjar Bhavan Page",
+		keyword: "freshers"
 	},
 	{
 		location: {lat: 21.162859,lng: 72.790060},
@@ -72,7 +80,8 @@ var initialPlaces = [
 		image: "images/svb.jpg",
 		description: "Supreme hostel of Svnit for final year boys...",
 		weblink: "http://svnit.ac.in/community/hostels/svb/index.html",
-		linkdata: "SVB Page"
+		linkdata: "SVB Page",
+		keyword: "swami vivekanand"
 	},
 	{
 		location: {lat: 21.160582,lng: 72.789373},
@@ -80,7 +89,8 @@ var initialPlaces = [
 		image: "images/tb.jpg",
 		description: "One of old hostels of SVNIT...",
 		weblink: "http://svnit.ac.in/community/hostels/tb/index.html",
-		linkdata: "Tagore Bhavan Page"
+		linkdata: "Tagore Bhavan Page",
+		keyword: "tagore"
 	},
 	{
 		location: {lat: 21.161848,lng: 72.790242},
@@ -88,7 +98,8 @@ var initialPlaces = [
 		image: "images/nehru.jpg",
 		description: "Hostel for the aspiring 2nd year boys...",
 		weblink: "http://svnit.ac.in/community/hostels/nb/index.html",
-		linkdata: "Nehru Bhavan Page"
+		linkdata: "Nehru Bhavan Page",
+		keyword: "nehru"
 	},
 	{
 		location: {lat: 21.162360,lng: 72.788231},
@@ -96,7 +107,8 @@ var initialPlaces = [
 		image: "images/rb.jpg",
 		description: "Home to all the married Teaching Assistants & Ph.D. students.",
 		weblink: "http://svnit.ac.in/community/hostels/rb/index.html",
-		linkdata: "Raman Bhavan Page"
+		linkdata: "Raman Bhavan Page",
+		keyword: "c v raman"
 	},
 	{
 		location: {lat: 21.163950,lng: 72.785132},
@@ -104,7 +116,8 @@ var initialPlaces = [
 		image: "images/ncrc.jpg",
 		description: "Epicle of Knowledge... Largest building at Svnit... where all lectures are held..",
 		weblink: "http://svnit.ac.in/jobs.php",
-		linkdata: "Teaching Jobs at SVNIT"
+		linkdata: "Teaching Jobs at SVNIT",
+		keyword: "engineering"
 	},
 	{
 		location: {lat: 21.167441,lng: 72.782845},
@@ -112,7 +125,8 @@ var initialPlaces = [
 		image: "images/guest.jpg",
 		description: "Primal stay for the guests and executives of companys coming for placements... with top class catering...",
 		weblink: "http://svnit.ac.in/campus/guesthouse.php",
-		linkdata: "SVNIT Guest House- Narmad Bhavan"
+		linkdata: "SVNIT Guest House- Narmad Bhavan",
+		keyword: "guest house"
 	},
 	{
 		location: {lat: 21.165000,lng: 72.783634},
@@ -120,7 +134,8 @@ var initialPlaces = [
 		image: "images/ccc.jpg",
 		description: "Data centre of Svnit... controls the supply of super high speed internet across the campus...",
 		weblink: "http://svnit.ac.in/resources/ccc/",
-		linkdata: "CCC"
+		linkdata: "CCC",
+		keyword: "cyberroam"
 	},
 	{
 		location: {lat: 21.164754,lng: 72.783314},
@@ -128,7 +143,8 @@ var initialPlaces = [
 		image: "images/coed.jpg",
 		description: "Department that gives the best placements every year... COED...",
 		weblink: "http://117.239.204.235/",
-		linkdata: "COED Home Page"
+		linkdata: "COED Home Page",
+		keyword: "computer engineering"
 	},
 	{
 		location: {lat: 21.164314,lng: 72.783585},
@@ -136,7 +152,8 @@ var initialPlaces = [
 		image: "images/eced.jpg",
 		description: "Department with aspiring talent in the field of electronics... ECED...",
 		weblink: "http://svnit.ac.in/deptt/eced/index.php",
-		linkdata: "ECED Home Page"
+		linkdata: "ECED Home Page",
+		keyword: "electronics engineering"
 	},
 	{
 		location: {lat: 21.164036,lng: 72.782995},
@@ -144,7 +161,8 @@ var initialPlaces = [
 		image: "images/ched.jpg",
 		description: "Department with new researches including Saline Water Treatment Plant... CHED...",
 		weblink: "http://svnit.ac.in/deptt/ched/index.php",
-		linkdata: "CHED Home Page"
+		linkdata: "CHED Home Page",
+		keyword: "chemical engineering"
 	},
 	{
 		location: {lat: 21.162041,lng: 72.783216},
@@ -152,7 +170,8 @@ var initialPlaces = [
 		image: "images/disp.jpg",
 		description: "Dispensary for students and staff with on the go treatment and free medication...",
 		weblink: "http://svnit.ac.in/healthcentre/healthcentre.php",
-		linkdata: "SVNIT Dispensary"
+		linkdata: "SVNIT Dispensary",
+		keyword: "dispensary"
 	}
 	// ,
 	// {
--- js/script.js
+++ js/script.js
@@ -6,18 +6,59 @@
 var Marker = function (data, gmap){
 	var self = this;
 
+	// Declaring variables for every Marker
 	this.title = ko.observable(data.title);
 	this.location = ko.observable(data.location);
+	this.keyword = ko.observable(data.keyword);
+	this.infocontent = ko.observable('');
+	this.wikilinks = ko.observable('');
 
-	// Creating window for info of every marker.
-	this.infowindow = new google.maps.InfoWindow({
-    	content: '<div id="content" class="text-center">'+
+	this.infocontent('<div id="content" class="text-center">'+
 		            '<h4 id="firstHeading" class="firstHeading">'+data.title+
-		            '</h4><div id="bodyContent"><p>'+data.description+'</p>'+
+		            '</h4><div id="bodyContent" data-bind="with: Marker"><p>'+data.description+'</p>'+
 		            '<img class="info_img text-center" src='+data.image+
-		            '><p>For More Details Visit:<a class="infolinks" target="_blank" href='
-		            +data.weblink+'>'+data.linkdata+'</a></p></div></div>'
-    });
+		            '><p>For More Details Visit:<a class="infolinks" target="_blank" href='+data.weblink+
+                '>'+data.linkdata+'</a></p><p data-bind="text: wikilinks">'+
+		            'Related Wikipedia Links - ');
+	// console.log(this.infowindow.content);
+
+	// Declaring url for keyword search on Wikipedia
+	this.wikiurl = 'https://en.wikipedia.org/w/api.php?action=opensearch&search='+self.keyword()+'&format=json&callback=wikicallback';
+
+	// Ajax Call for getting data from Wikipedia
+	$.ajax( {
+	    url: self.wikiurl,
+	    dataType: 'jsonp',
+	    type: 'POST',
+	    async: true,
+	    headers: { 'Api-User-Agent': 'Example/1.0' },
+	    success: function(data) {
+			links = data[1];
+			// console.log(links);
+			// console.log(data);
+			if (links.length>3){
+				linkcount = 3;
+			}
+			else{
+				linkcount = links.length;
+			}
+			for (var i=0;i<linkcount;i++){
+			    var link = links[i];
+			    self.infocontent(self.infocontent()+'<a class="wikilinks" target="_blank" href="https://en.wikipedia.org/wiki/'
+			    	+link+'">'+link+'</a>...');
+			}
+			self.infocontent(self.infocontent()+'</p></div></div>');
+			console.log('Wikipedia Links added successfully');
+			// clearTimeout(self.wikiRequestTimeout);
+		}
+	});
+
+	// Using timeout to set content in Infowindow
+	this.wikiRequestTimeout = setTimeout(function(){
+		self.infowindow = new google.maps.InfoWindow({
+    		content: self.infocontent()
+   		});
+	}, 7000);
 
 	// Creating Marker for every location.
 	this.marker = new google.maps.Marker({
@@ -28,7 +69,7 @@ var Marker = function (data, gmap){
 	});
 	// Adding Event Listeners for Animation and Infowindow.
 	this.marker.addListener('click', toggleBounce);
-
+	// console.log(self.infowindow.content);
 	this.marker.addListener('click', function() {
 		self.infowindow.open(map, this);
 	});
@@ -45,7 +86,7 @@ var Marker = function (data, gmap){
         	self.marker.setAnimation(google.maps.Animation.BOUNCE);
         	setTimeout(function(){ self.marker.setAnimation(null); }, 4200);
         }
-    };
+    }
     
 };
 
@@ -53,6 +94,7 @@ var Marker = function (data, gmap){
 var ViewModel = function (){
 	var self = this;
 
+	//Definig map to be rendered onload
 	this.map = new google.maps.Map(document.getElementById('map'), {
 		zoom: 16,
 		center: {lat: 21.162840,lng: 72.785203}
@@ -63,6 +105,7 @@ var ViewModel = function (){
     initialPlaces.forEach( function(place, map) {
 		self.placeList.push(new Marker(place, self.map));
 	});
+	// console.log(self.placeList());
 	this.locsearch = ko.observable("");
 
 	this.filterList = ko.observableArray([]);
@@ -80,9 +123,10 @@ var ViewModel = function (){
 				self.filterList.push({name: self.placeList()[i].title()});
 			}
 
-		};
+		}
 		// console.log(self.filterList());
 	};
+	
 	// A function that pops up the infowindow of selected location.
 	this.changemarker = function (clickedloc){
 		for (var i = self.placeList().length - 1; i >= 0; i--) {
@@ -94,16 +138,59 @@ var ViewModel = function (){
 				setTimeout(function(){
 					for (var j = self.placeList().length - 1; j >= 0; j--) {
 						self.placeList()[j].marker.setAnimation(null); 
-					};
+					}
 				}, 4200);
 			}
 			else{
 				self.placeList()[i].infowindow.close();
 				self.placeList()[i].marker.setAnimation(null);
 			}
-		};
+		}
 	};	
 	console.log('ViewModel Working');
+
+	this.gourl = "http://developer.goibibo.com/api/voyager/get_hotels_by_cityid/?app_id=ead7e995&app_key=f086131766bdeeb9b60ab56a0d7b9372&city_id=1174752501934903427"
+	this.hotels = ko.observableArray([]);
+	// Setting Error function for GoIbibo
+	this.goibiboRequestTimeout = setTimeout(function(){
+			console.log("Failed to get GoIbibo Hotel Feed");
+	    }, 12000);
+
+	// Ajax Call for getting data of Hotels
+	$.ajax( {
+	url: self.gourl,
+	dataType: 'jsonp',
+	type: 'GET',
+	async: true,
+	success: function(response) {
+		this.links = response.data;
+		console.log(this.links);
+		var names = [];
+		var i = 0;
+		for (this.hotelid in this.links){
+			i++;
+			this.hotel = response.data[this.hotelid].hotel_data_node;
+			// console.log(this.hotel.loc.location+","+this.hotel.loc.city);
+			if(this.hotel.img_selected){
+				self.hotels.push({
+					name: this.hotel.name,
+					price: this.hotel.extra.check_in,
+					image: this.hotel.img_selected.g.l,
+					link:'https://www.goibibo.com/hotels/detail/'+this.hotel.name+'-hotel-in-surat-'+this.hotelid+'/',
+					address: this.hotel.loc.location
+				});
+			}
+			else{
+				console.log("Error- No image defined for hotel - "+hotelid);
+				continue;
+			}
+			};
+			clearTimeout(self.goibiboRequestTimeout);
+			console.log("GO Ibibo Hotels added");
+			// console.log(self.hotels());
+		}
+	});
+	console.log('API file loaded');
 };
 
 //Callback Function to initialise Google Map alongwith Knockout Bindings.
